1c27fd4eafbe008f4d054d080529fc5602aa6d82
Hamdaan Khalid <hamdaankhalid@gmail.com>
2025-09-05 09:22:15 -0700

    init

---
+++ BaddieFs.cs
@@ -0,0 +1,167 @@
+﻿using BaddieFs.passthrough;
+using Fsp;
+using Fsp.Interop;
+using System.Security.AccessControl;
+
+namespace BaddieFs
+{
+    internal class BaddieFs : Ptfs 
+    {
+        public BaddieFs(string mirrorDir) : base(mirrorDir)
+        {}
+    }
+
+
+    // Taken from PtfsService.cs in passthrough-dotnet
+    class BaddieFsService : Service
+    {
+        private class CommandLineUsageException : Exception
+        {
+            public CommandLineUsageException(String Message = null) : base(Message)
+            {
+                HasMessage = null != Message;
+            }
+
+            public bool HasMessage;
+        }
+
+        private const String PROGNAME = "baddiefs-dotnet";
+
+        public BaddieFsService() : base("BaddieFsService")
+        {
+        }
+
+        protected override void OnStart(String[] Args)
+        {
+            try
+            {
+                String DebugLogFile = null;
+                UInt32 DebugFlags = 0;
+                String VolumePrefix = null;
+                String PassThrough = null;
+                String MountPoint = null;
+                IntPtr DebugLogHandle = (IntPtr)(-1);
+                FileSystemHost Host = null;
+                Ptfs Ptfs = null;
+                int I;
+
+                for (I = 1; Args.Length > I; I++)
+                {
+                    String Arg = Args[I];
+                    if ('-' != Arg[0])
+                        break;
+                    switch (Arg[1])
+                    {
+                        case '?':
+                            throw new CommandLineUsageException();
+                        case 'd':
+                            argtol(Args, ref I, ref DebugFlags);
+                            break;
+                        case 'D':
+                            argtos(Args, ref I, ref DebugLogFile);
+                            break;
+                        case 'm':
+                            argtos(Args, ref I, ref MountPoint);
+                            break;
+                        case 'p':
+                            argtos(Args, ref I, ref PassThrough);
+                            break;
+                        case 'u':
+                            argtos(Args, ref I, ref VolumePrefix);
+                            break;
+                        default:
+                            throw new CommandLineUsageException();
+                    }
+                }
+
+                if (Args.Length > I)
+                    throw new CommandLineUsageException();
+
+                if (null == PassThrough && null != VolumePrefix)
+                {
+                    I = VolumePrefix.IndexOf('\\');
+                    if (-1 != I && VolumePrefix.Length > I && '\\' != VolumePrefix[I + 1])
+                    {
+                        I = VolumePrefix.IndexOf('\\', I + 1);
+                        if (-1 != I &&
+                            VolumePrefix.Length > I + 1 &&
+                            (
+                            ('A' <= VolumePrefix[I + 1] && VolumePrefix[I + 1] <= 'Z') ||
+                            ('a' <= VolumePrefix[I + 1] && VolumePrefix[I + 1] <= 'z')
+                            ) &&
+                            '$' == VolumePrefix[I + 2])
+                        {
+                            PassThrough = String.Format("{0}:{1}", VolumePrefix[I + 1], VolumePrefix.Substring(I + 3));
+                        }
+                    }
+                }
+
+                if (null == PassThrough || null == MountPoint)
+                    throw new CommandLineUsageException();
+
+                if (null != DebugLogFile)
+                    if (0 > FileSystemHost.SetDebugLogFile(DebugLogFile))
+                        throw new CommandLineUsageException("cannot open debug log file");
+
+                Host = new FileSystemHost(new BaddieFs("C://"));
+
+                Host.Prefix = VolumePrefix;
+                if (0 > Host.Mount(MountPoint, null, true, DebugFlags))
+                    throw new IOException("cannot mount file system");
+                MountPoint = Host.MountPoint();
+                _Host = Host;
+
+                Log(EVENTLOG_INFORMATION_TYPE, String.Format("{0}{1}{2} -p {3} -m {4}",
+                    PROGNAME,
+                    null != VolumePrefix && 0 < VolumePrefix.Length ? " -u " : "",
+                        null != VolumePrefix && 0 < VolumePrefix.Length ? VolumePrefix : "",
+                    PassThrough,
+                    MountPoint));
+            }
+            catch (CommandLineUsageException ex)
+            {
+                Log(EVENTLOG_ERROR_TYPE, String.Format(
+                    "{0}" +
+                    "usage: {1} OPTIONS\n" +
+                    "\n" +
+                    "options:\n" +
+                    "    -d DebugFlags       [-1: enable all debug logs]\n" +
+                    "    -D DebugLogFile     [file path; use - for stderr]\n" +
+                    "    -u \\Server\\Share    [UNC prefix (single backslash)]\n" +
+                    "    -p Directory        [directory to expose as pass through file system]\n" +
+                    "    -m MountPoint       [X:|*|directory]\n",
+                    ex.HasMessage ? ex.Message + "\n" : "",
+                    PROGNAME));
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Log(EVENTLOG_ERROR_TYPE, String.Format("{0}", ex.Message));
+                throw;
+            }
+        }
+        protected override void OnStop()
+        {
+            _Host.Unmount();
+            _Host = null;
+        }
+
+        private static void argtos(String[] Args, ref int I, ref String V)
+        {
+            if (Args.Length > ++I)
+                V = Args[I];
+            else
+                throw new CommandLineUsageException();
+        }
+        private static void argtol(String[] Args, ref int I, ref UInt32 V)
+        {
+            Int32 R;
+            if (Args.Length > ++I)
+                V = Int32.TryParse(Args[I], out R) ? (UInt32)R : V;
+            else
+                throw new CommandLineUsageException();
+        }
+
+        private FileSystemHost _Host;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Fsp;
+
+namespace BaddieFs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaddieFsService service = new BaddieFsService();
+            Environment.ExitCode = service.Run();
+        }
+    }
+}
